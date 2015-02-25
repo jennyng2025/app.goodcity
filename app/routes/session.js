40a5e9c019ac6e20c0829d8f94e4b2de2c660ec1
swatijadhav <swati.jadhav@kiprosh.com>
2015-02-25 19:29:00 +0530

    moved common shared routes to goodcity-addon

--- app/routes/session.js
+++ app/routes/session.js
@@ -3,17 +3,7 @@ import Ember from 'ember';
 var SessionRoute = Ember.Route.extend({
   beforeModel: function() {
     if (this.controllerFor('application').get('isLoggedIn')) {
-      var currentUser = this.get('session.currentUser');
-      if (currentUser.get('isStaff')) {
-        var myOffers = this.store.all('offer').filterBy('reviewedBy.id', currentUser.get('id'));
-        if(myOffers.get('length') > 0) {
-          this.transitionTo('inbox.my_list');
-        } else {
-          this.transitionTo('inbox');
-        }
-      } else {
-        this.transitionTo('/offers');
-      }
+      this.transitionTo('/offers');
     }
   }
 });
