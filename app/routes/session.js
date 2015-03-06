f71f583842d5951c5476691967900be5194710f3
Michael McDonald <mcm-ham@users.noreply.github.com>
2015-03-06 16:55:46 +0800

    Move isLoggedIn status to session

--- app/routes/session.js
+++ app/routes/session.js
@@ -1,11 +1,9 @@
 import Ember from 'ember';
 
-var SessionRoute = Ember.Route.extend({
+export default Ember.Route.extend({
   beforeModel: function() {
-    if (this.controllerFor('application').get('isLoggedIn')) {
+    if (this.session.get('isLoggedIn')) {
       this.transitionTo('/offers');
     }
   }
 });
-
-export default SessionRoute;
