8feac002488cfa223244c83648bdda853b130349
Michael McDonald <mcm-ham@users.noreply.github.com>
2015-03-12 13:52:16 +0800

    Only include jquery.mockjax.js for test environment

--- Brocfile.js
+++ Brocfile.js
@@ -84,6 +84,6 @@ app.import({
   }
 });
 
-app.import('bower_components/jquery-mockjax/jquery.mockjax.js');
+app.import({test:'bower_components/jquery-mockjax/jquery.mockjax.js'});
 
 module.exports = app.toTree();
