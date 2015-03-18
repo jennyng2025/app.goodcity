e01fbb6a834bae749714ca39b828b5af7285dc11
Michael McDonald <mcm-ham@users.noreply.github.com>
2015-03-18 14:21:46 +0800

    GCW-190 always output what shared.goodcity branch is being used

--- circle-branch.js
+++ circle-branch.js
@@ -7,13 +7,20 @@ function exec(cmd) {
 
 var currentBranch = exec("git rev-parse --abbrev-ref HEAD");
 currentBranch = (/GCW-\d+/.exec(currentBranch) || [])[0];
-if (!currentBranch) return;
+if (!currentBranch) {
+  console.log("shared.goodcity using master");
+  return;
+}
 
 var branches = exec("curl https://api.github.com/repos/crossroads/" + repoName + "/branches");
 var matched = JSON.parse(branches)
   .map(function(b) { return b.name; })
   .filter(function(name) { return name.indexOf(currentBranch) !== -1; })[0];
-if (!matched) return;
 
-console.log("Switching shared.goodcity to " + matched + " branch");
+if (!matched) {
+  console.log("shared.goodcity using master");
+  return;
+}
+
+console.log("shared.goodcity using " + matched);
 exec("sed -i 's/shared\.goodcity\.git/shared.goodcity.git#'" + matched + "'/g' package.json");
