56ac3c7fa3dad922677e7131d280a1a62fb7e3d9
Michael McDonald <mcm-ham@users.noreply.github.com>
2015-03-24 12:43:57 +0800

    Fix issue where new offers are created when we already have an empty offer

--- app/routes/offers/new.js
+++ app/routes/offers/new.js
@@ -2,8 +2,12 @@ import AuthorizeRoute from '../authorize';
 
 export default AuthorizeRoute.extend({
   model: function() {
-    var offer = this.store.createRecord('offer');
+    var offer = this.store.all('offer').filterBy('itemCount', 0).get('firstObject');
+    if (offer) {
+      return this.transitionTo('offer', offer);
+    }
 
+    offer = this.store.createRecord('offer');
     offer.save()
       .then(() => this.transitionTo('offer', offer.id))
       .catch(error => {
