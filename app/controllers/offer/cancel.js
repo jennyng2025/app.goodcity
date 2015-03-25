e84ea7685fea841fcf8048e10eaf2dec5e25c579
swatijadhav <swati.jadhav@kiprosh.com>
2015-03-25 20:55:49 +0530

    move gogovan-contact to config

--- app/controllers/offer/cancel.js
+++ app/controllers/offer/cancel.js
@@ -1,10 +1,12 @@
 import Ember from 'ember';
+import config from '../../config/environment';
+
 export default Ember.ObjectController.extend({
   needs: ['offer/offer_details'],
 
   canCancel: Ember.computed.alias('model.delivery.gogovanOrder.isCancelled'),
-
   driverContact: Ember.computed.alias('model.delivery.gogovanOrder.driverMobile'),
+  gogovanContact: config.APP.GOGOVAN_CONTACT,
 
   actions: {
     cancelOffer: function() {
