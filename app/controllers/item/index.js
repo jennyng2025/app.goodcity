a043de5a203c40ad350387f8af782ff76caf0c51
sunil sharma <sunil.sharma@kiprosh.com>
2015-04-17 17:57:10 +0530

    Missing property

--- app/controllers/item/index.js
+++ app/controllers/item/index.js
@@ -1,6 +1,7 @@
 import Ember from "ember";
 
 export default Ember.Controller.extend({
+  offerDetailsController: Ember.inject.controller("offer/offer_details")
 
   actions: {
     removeItem: function(item) {
