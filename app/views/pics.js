b6be27dea73d30bf74b18e662c8d39dcff697621
swatijadhav <swati.jadhav@kiprosh.com>
2015-05-04 12:10:42 +0530

    pics page linked from register page

---
+++ app/views/pics.js
@@ -0,0 +1,33 @@
+import Ember from 'ember';
+
+export default Ember.View.reopen({
+  didInsertElement: function() {
+    this._super();
+    Ember.$(document).foundation();
+
+    Ember.$().ready(function(){
+      backToTopLink();
+    });
+
+    // Back-to-top Link
+    function backToTopLink(){
+      var offset = 300;
+      var duration = 300;
+
+      Ember.$(window).scroll(function() {
+        if (Ember.$(this).scrollTop() > offset) {
+          Ember.$('.back-to-top').fadeIn(duration);
+        } else {
+          Ember.$('.back-to-top').fadeOut(duration);
+        }
+      });
+
+      Ember.$('.back-to-top').click(function() {
+        Ember.$('html, body').animate({scrollTop: 0}, duration);
+        return false;
+      });
+    }
+
+  },
+
+});
