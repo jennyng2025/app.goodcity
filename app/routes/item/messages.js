96b5a11e76f5ac7b2915a1f063e4dea8725dfb46
Michael McDonald <mcm-ham@users.noreply.github.com>
2015-07-28 16:45:12 +0800

    GCW-520 hide supervisor messages that appear when a staff user is logged in donor app

--- app/routes/item/messages.js
+++ app/routes/item/messages.js
@@ -4,6 +4,7 @@ export default ReadMessagesRoute.extend({
   model: function() {
     var itemId = this.modelFor('item').get('id');
     return this.store.filter('message', function(message) {
+      // isPrivate is checked because a reviewer logged in donor app will still receive messages intended for supervisor thread
       return message.get('item.id') === itemId && message.get('isPrivate') === false;
     });
   }
