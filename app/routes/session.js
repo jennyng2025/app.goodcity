61cc1c93f627ceb0e28786c820d5bd49aa75d9f9
swatijadhav <swati.jadhav@kiprosh.com>
2015-09-21 23:16:14 +0530

    handle no internet-connection

--- app/routes/session.js
+++ app/routes/session.js
@@ -1,9 +1,14 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+
   beforeModel: function() {
     if (this.session.get('isLoggedIn')) {
-      this.transitionTo('/offers');
+      if(this.get('session.currentUser')) {
+        this.transitionTo('/offers');
+      } else {
+        this.container.lookup("route:application")._loadDataStore();
+      }
     }
   }
 });
