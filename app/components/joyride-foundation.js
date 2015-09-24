97722f2c21bda4dee758bd9dba9fc11be3ac836b
swatijadhav <swati.jadhav@kiprosh.com>
2015-09-24 20:38:24 +0530

    replace all with peekAll

--- app/components/joyride-foundation.js
+++ app/components/joyride-foundation.js
@@ -6,8 +6,8 @@ export default initFoundation.extend({
   store: Ember.inject.service(),
 
   didInsertElement() {
-    var offerCount = this.get("store").all("offer").get("length");
-    var itemCount = this.get("store").all("item").get("length");
+    var offerCount = this.get("store").peekAll("offer").get("length");
+    var itemCount = this.get("store").peekAll("item").get("length");
     var recentlyCreated = new Date() - this.get("_controller.model.createdAt") <= 12 * 60 * 60 * 1000; // 12 hrs
     var firstEverItem = offerCount === 1 && itemCount === 1 && recentlyCreated;
 
