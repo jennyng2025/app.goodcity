f8ca492e2877b8699178f8b3be7b0df728de1749
swatijadhav <swati.jadhav@kiprosh.com>
2015-10-28 14:04:29 +0530

    update authenticate route

---
+++ app/routes/authenticate.js
@@ -0,0 +1,14 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+
+  beforeModel() {
+    if (this.session.get('isLoggedIn')) {
+      if(this.get('session.currentUser')) {
+        this.transitionTo('/offers');
+      } else {
+        this.container.lookup("route:application")._loadDataStore();
+      }
+    }
+  }
+});
