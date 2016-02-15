005fbbf5e072cb1d65df1431f509464cdadca39a
swatijadhav <swati.jadhav@kiprosh.com>
2016-02-15 13:31:27 +0530

    language-support for uservoice widget and load user-details in uservoice-form

--- app/components/user-voice.js
+++ app/components/user-voice.js
@@ -2,12 +2,24 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
 
+  isChinese: Ember.computed('session.language', function(){
+    return this.get('session.language') === 'zh-tw';
+  }),
+
   didInsertElement() {
     var height = (Ember.$(window).height() - 82) + "px";
+    var locale = this.get("isChinese") ? "zh-TW" : "en";
+    var name = this.get("session.currentUser.fullName") || "";
+    var mobile = this.get("session.currentUser.mobileWithCountryCode") || "";
 
     UserVoice.push(['set', {
+      locale: locale,
 
       contact_enabled: true,
+      ticket_custom_fields: {
+        'Name': name,
+        'Mobile': mobile
+      },
 
       strings: {
         // Menu items
