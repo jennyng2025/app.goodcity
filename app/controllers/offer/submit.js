468e181964ea1cbe3ab5b703a117c5aae292296a
swatijadhav <swati.jadhav@kiprosh.com>
2016-03-10 20:55:13 +0530

    removed instance of store.push

--- app/controllers/offer/submit.js
+++ app/controllers/offer/submit.js
@@ -7,11 +7,8 @@ export default Ember.Controller.extend({
   actions: {
     submitOffer(saleable) {
       var loadingView = this.container.lookup('component:loading').append();
-      var offer = this.store.push('offer', {
-        id: this.get('model.id'),
-        state_event: 'submit',
-        saleable: saleable
-      });
+      var offer = this.get("model");
+      offer.setProperties({ state_event: 'submit', saleable: saleable });
 
       offer.save()
         .then(() => this.transitionToRoute('offer.offer_details'))
