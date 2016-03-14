454a5b820d07857c04db5985fdb35b46a7eb83a5
swatijadhav <swati.jadhav@kiprosh.com>
2016-03-14 20:00:13 +0530

    update ember and ember-data to v2.2.0 & updated test-cases

--- server/mocks/offers.js
+++ server/mocks/offers.js
@@ -35,6 +35,34 @@ module.exports = function(app) {
     ]
   };
 
+  var offer_json = {
+    "offer": { "id": "1", "state": "draft", "item_ids": ["1", "2"], "delivery_id": null },
+    "items": [
+      { "id": "1", "donor_description": "example1", "offer_id": "1", "image_ids": ["1", "2"] },
+      { "id": "2", "donor_description": "example2", "offer_id": "1" },
+      { "id": "4", "donor_description": "Velit fugit amet quos ut minima quis", "offer_id": "4", "donor_condition_id": "1", "image_ids": ["1"], "state": "submitted" },
+      { "id": "5", "donor_description": "test", "offer_id": "4", "donor_condition_id": "1", "image_ids": ["1"], "state": "accepted" }
+    ],
+    "images": [
+      { "id": "1", "favourite": "false", "cloudinary_id": image_id },
+      { "id": "2", "favourite": "true", "cloudinary_id": image_id }
+    ],
+    "addresses": [
+      { "id": 1, "street": null, "flat": null, "building": null, "district_id": null, "addressable_type": "User", "addressable_id": 1 }
+    ],
+    "messages": [
+      { "id": "1", "body": "I have made an offer.", "state": null, "sender_id": "1", "is_private": false, "offer_id": "1", "item_id": null }
+    ],
+    "users": [
+      { "id": "1", "first_name": "Kendrick", "last_name": "Kiehn", "permission_id": null },
+      { "id": "2", "first_name": "Jaleel", "last_name": "Ondricka", "permission_id": "2" }
+    ]
+  };
+
+  var updated_offer_json = {
+    "offer": { "id": "101", "state": "submitted", "item_ids": ["1", "2"], "delivery_id": null },
+  }
+
   offersRouter.get("/", function(req, res) {
     res.send(offers_json);
   });
@@ -45,7 +73,7 @@ module.exports = function(app) {
   });
 
   offersRouter.get("/:id", function(req, res) {
-    res.send(offers_json);
+    res.send(offer_json);
   });
 
   offersRouter.delete("/:id", function(req, res) {
@@ -53,7 +81,7 @@ module.exports = function(app) {
   });
 
   offersRouter.put("/:id", function(req, res) {
-    res.send(offers_json);
+    res.send(updated_offer_json);
   });
 
   offersRouter.post("/", function(req, res) {
