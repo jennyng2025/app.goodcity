a8765a5bc901b87f7ca9bcc9959346cf7aca728f
Michael McDonald <mcm-ham@users.noreply.github.com>
2016-03-19 15:44:01 +0800

    Remove loading of messages in route since it's now handled in controller Call super in setupController to allow parent routes implementation to change

--- app/routes/item/messages.js
+++ app/routes/item/messages.js
@@ -1,21 +1,8 @@
-import ReadMessagesRoute from './../read_messages';
+import MessagesBaseRoute from 'shared-goodcity/routes/messages_base';
 
-export default ReadMessagesRoute.extend({
-  item: null,
-
-  model() {
-    var itemId = this.modelFor('item').get('id');
-    var item = this.store.peekRecord('item', itemId);
-    this.set('item', item);
-
-    return this.store.filter('message', function(message) {
-      // isPrivate is checked because a reviewer logged in donor app will still receive messages intended for supervisor thread
-      return message.get('item.id') === itemId && message.get('isPrivate') === false;
-    });
-  },
-
+export default MessagesBaseRoute.extend({
   setupController(controller, model) {
-    controller.set('model', model);
-    controller.set('item', this.get('item'));
-  },
+    this._super(controller, model);
+    controller.set('item', this.modelFor('item'));
+  }
 });
